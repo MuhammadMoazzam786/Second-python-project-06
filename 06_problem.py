3f5c247beabb9b682807aaf09d40f8814b7abaf9
Muhammad Moazzam <295858777+MuhammadMoazzam786@users.noreply.github.com>
2026-06-27 20:34:27 +0500

    Add files via upload

---
+++ 06_problem.py
@@ -0,0 +1,6 @@
+a = int (input ("Enter your number 1: "))
+
+
+print ("The square of number",a**2)
+print ("The square of number",a*a)
+print ("The square of number",a^a)# incorrect the finding square of a number in python
